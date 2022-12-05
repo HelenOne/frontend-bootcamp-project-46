85ff8b5ea7c6f616692acf86523f1438bc34e041
HelenOne <h.bright@yandex.ru>
2022-12-05 04:19:46 +0300

    made step 2

--- gendiff.js
+++ gendiff.js
@@ -2,8 +2,12 @@ const { Command } = require("commander");
 const program = new Command();
 
 program
-  .description("Usage: gendiff [options]")
-  .version("1.0.0");
+  .description(
+    `Usage: gendiff [options] <filepath1> <filepath2>
 
+Compares two configuration files and shows a difference.`
+  )
+  .version("1.0.0")
+  .option("-f, --format <type>", "output format")
 
 program.parse();
