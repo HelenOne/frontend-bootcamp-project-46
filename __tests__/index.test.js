2b83405604493b5e53471a61fe1b2ed027fa1bf7
HelenOne <h.bright@yandex.ru>
2022-12-12 03:39:50 +0300

    fixed tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,20 +1,23 @@
 import { test, expect } from "@jest/globals";
 import genDiff from "../index.js";
-import _ from 'lodash';
+import _ from "lodash";
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
 
-test("My first test", () => {
-  expect(10).toBe(10);
-});
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) =>
+  path.join(__dirname, "..", "__fixtures__", filename);
+const readFile = (filename) =>
+  fs.readFileSync(getFixturePath(filename), "utf-8");
+
+const file1 = getFixturePath("file1.json");
+const file2 = getFixturePath("file2.json");
 
 test("Compare plain objects", () => {
-  const expected = genDiff('./__fixtures__/file1.json', './__fixtures__/file2.json');
-  const actual = {
-  '- follow': false,
-  '  host': 'hexlet.io',
-  '- proxy': '123.234.53.22',
-  '- timeout': 50,
-  '+ timeout': 20,
-  '+ verbose': true
-};
-  expect(_.isEqual(expected, actual)).toBe(true);
+  const expected = genDiff(file1, file2);
+  const actual = readFile("diff.json");
+  expect(expected).toEqual(JSON.parse(actual));
 });
