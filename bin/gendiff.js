272bf532fb9ef6907967eaf0c530a5a515e42c9d
HelenOne <h.bright@yandex.ru>
2022-12-25 18:32:48 +0300

    added stylish format by default

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,7 +10,7 @@ program
 Compares two configuration files and shows a difference.`
   )
   .version("1.0.0")
-  .option("-f, --format <type>", "output format")
+  .option("-f, --format <type>", "output format", "stylish")
   .argument("<filepath1>")
   .argument("<filepath2>")
   .action((filepath1, filepath2) => {
